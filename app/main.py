a1819de27756b3de9184769ddb997d6672830330
dmitrymashkalo <dmitrymashkalo@gmail.com>
2025-05-08 14:09:54 +0200

    Add config, .env, db

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.db.db import get_db
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def read_root(db: AsyncSession = Depends(get_db)):
+    result = await db.execute(text("SELECT 'Hello from DB'"))
+    return {"message": result.scalar()}
