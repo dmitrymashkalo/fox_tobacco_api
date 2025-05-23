c81453d4deece96fdc206ddcc09c1d62db24a13b
dmitrymashkalo <dmitrymashkalo@gmail.com>
2025-05-23 19:48:34 +0200

    small fix

--- app/db/db.py
+++ app/db/db.py
@@ -4,7 +4,7 @@ from app.config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=True, connect_args={"statement_cache_size": 0})
 
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
