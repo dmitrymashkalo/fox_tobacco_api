05be1f16e845541f61c8e49ac800d7ae796508e0
dmitrymashkalo <dmitrymashkalo@gmail.com>
2025-05-23 21:49:55 +0200

    fixes

--- app/db/db.py
+++ app/db/db.py
@@ -1,16 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
-import os
-import logging
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-logger.info(f"DEBUG: DB_HOST read from env: '{DB_HOST}'")
-logger.info(f"DEBUG: DATABASE_URL will try to connect to: postgresql+asyncpg://{DB_USER}:***@{DB_HOST}:{DB_PORT}/{DB_NAME}")
-
-
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}"
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}?sslmode=require"
 
 engine = create_async_engine(
     DATABASE_URL,
@@ -18,7 +10,11 @@ engine = create_async_engine(
     connect_args={"statement_cache_size": 0}
 )
 
-AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
 
 
 async def get_db():
