ad531a42eb9ebe65c28815251b07720cf6d4bb78
dmitrymashkalo <dmitrymashkalo@gmail.com>
2025-05-23 21:14:57 +0200

    test

--- app/db/db.py
+++ app/db/db.py
@@ -1,6 +1,14 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
+import os
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.info(f"DEBUG: DB_HOST read from env: '{DB_HOST}'")
+logger.info(f"DEBUG: DATABASE_URL will try to connect to: postgresql+asyncpg://{DB_USER}:***@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}"
 
