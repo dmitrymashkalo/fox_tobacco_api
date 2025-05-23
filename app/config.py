f157ccc5a7629395887b7faf0b864bb9639d41c0
dmitrymashkalo <dmitrymashkalo@gmail.com>
2025-05-23 19:40:34 +0200

    small fixes

--- app/config.py
+++ app/config.py
@@ -3,14 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-
-user = os.getenv("user")
-password = os.getenv("password")
-host = os.getenv("host")
-port = os.getenv("port")
-dbname = os.getenv("dbname")
+DB_USER = os.getenv("user")
+DB_PASS = os.getenv("password")
+DB_HOST = os.getenv("host")
+DB_PORT = os.getenv("port")
+DB_NAME = os.getenv("dbname")
