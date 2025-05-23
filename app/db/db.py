96a5f4cd4b9da51a4dd4db66fa02bdc26e52010b
dmitrymashkalo <dmitrymashkalo@gmail.com>
2025-05-24 01:14:07 +0200

    fix connection db issue

--- app/db/db.py
+++ app/db/db.py
@@ -14,38 +14,42 @@ if not all([DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME]):
     logger.error("Missing one or more PostgreSQL environment variables!")
     raise ValueError("Missing one or more PostgreSQL environment variables!")
 
-
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 CERT_PATH = os.path.join(PROJECT_ROOT, "certs", "supabase_ca.crt")
 
-logger.info(f"Attempting to use SSL certificate from: {CERT_PATH}") # logs
+logger.info(f"Attempting to use SSL certificate from: {CERT_PATH}")
 
 # cerf exists check
 if not os.path.exists(CERT_PATH):
     logger.error(f"SSL certificate file not found at: {CERT_PATH}")
     raise FileNotFoundError(f"SSL certificate file not found at: {CERT_PATH}")
 
-
 ssl_context = ssl.create_default_context(
     cafile=CERT_PATH
 )
-ssl_context.verify_mode = ssl.CERT_REQUIRED # Убедитесь, что проверка включена
-
+ssl_context.verify_mode = ssl.CERT_REQUIRED
+ssl_context.check_hostname = False
 
 engine = create_async_engine(
     DATABASE_URL,
+    echo=False,
     connect_args={
-        "ssl": ssl_context
-    }
+        "ssl": ssl_context,
+        "timeout": 30,
+    },
+    pool_size=10,
+    max_overflow=0,
+    pool_recycle=55,
+    pool_pre_ping=True,
 )
 
 AsyncSessionLocal = sessionmaker(
     bind=engine,
     class_=AsyncSession,
-    autocommit=False, # <-- Добавлено
-    autoflush=False,  # <-- Добавлено
+    autocommit=False,
+    autoflush=False,
     expire_on_commit=False
 )
 
