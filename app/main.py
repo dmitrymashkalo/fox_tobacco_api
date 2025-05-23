47259ef21a7372a21fa6875699dbe2bba630be7e
dmitrymashkalo <dmitrymashkalo@gmail.com>
2025-05-23 22:03:36 +0200

    delete trash

--- app/main.py
+++ app/main.py
@@ -10,14 +10,6 @@ app = FastAPI()
 app.include_router(brand.router)
 app.include_router(flavor.router)
 
-
-#@app.on_event("startup")
-#async def startup():
-    # Create all tables
-    # async with engine.begin() as conn:
-    #    await conn.run_sync(Base.metadata.create_all)
-
-
 @app.get("/")
 async def redirect():
     return RedirectResponse("/docs")
